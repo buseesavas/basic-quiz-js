b11bc653a6da26707e93311491f701e379277a13
Buse Savaş <81926506+buseesavas@users.noreply.github.com>
2024-10-07 02:00:29 +0300

    Add files via upload

---
+++ assets/js/quiz.js
@@ -0,0 +1,95 @@
+
+let dogruCevapSayisi = 0;
+let yanlisCevapSayisi = 0;
+
+let soru1 = prompt("10 + 21 = ?");
+if (soru1 === "31") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 31.");
+  yanlisCevapSayisi++;
+}
+
+let soru2 = prompt("12 + 25 = ?");
+if (soru2 === "37") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 37.");
+  yanlisCevapSayisi++;
+}
+
+let soru3 = prompt("23 + 12 = ?");
+if (soru3 === "35") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 35.");
+  yanlisCevapSayisi++;
+}
+
+let soru4 = prompt("20 + 16 = ?");
+if (soru4 === "36") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 36.");
+  yanlisCevapSayisi++;
+}
+
+let soru5 = prompt("32 + 8 = ?");
+if (soru5 === "40") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 40.");
+  yanlisCevapSayisi++;
+}
+
+let soru6 = prompt("18 + 8 = ?");
+if (soru6 === "26") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 26.");
+  yanlisCevapSayisi++;
+}
+
+let soru7 = prompt("6 + 8 = ?");
+if (soru7 === "14") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 14.");
+  yanlisCevapSayisi++;
+}
+
+let soru8 = prompt("9 + 7 = ?");
+if (soru8 === "16") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 16.");
+  yanlisCevapSayisi++;
+}
+
+let soru9 = prompt("10 + 8 = ?");
+if (soru9 === "18") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 18.");
+  yanlisCevapSayisi++;
+}
+
+let soru10 = prompt("13 + 12 = ?");
+if (soru10 === "25") {
+  alert("Doğru!");
+  dogruCevapSayisi++;
+} else {
+  alert("Yanlış cevap! Doğru cevap = 25.");
+  yanlisCevapSayisi++;
+}
+
+alert('Test bitti tebrikler! Doğru sayısı: ' + dogruCevapSayisi + ' Yanlış sayısı: ' + yanlisCevapSayisi);
